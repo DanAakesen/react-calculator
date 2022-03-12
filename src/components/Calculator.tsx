07e5c43b86b246b916150e464776bf804dc83156
Dan Rugtved Aakesen <dra@delegate.dk>
2022-03-12 15:19:48 +0100

    second comit

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: #323232;
+  flex: 1;
+`;
+
+const Calculator: React.FC<{}> = () => {
+  return <Container>Caclulator Comp</Container>;
+};
+
+export default Calculator;
